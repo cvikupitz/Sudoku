76d7620de40fbb7296471e96a2ca763392aa6e00
cvikupitz <cvikupit@uoregon.edu>
2016-10-29 13:31:56 -0700

    Initial commit, added main class.

---
+++ Main.java
@@ -0,0 +1,15 @@
+/**
+ * Main.java
+ * Authors: Lucas Chavarria, Cole Vikupitz, Ron Guo, James Xu
+ * -----------------------------------------------------------------------------
+ * Contains the main method that is invoked when the user starts the program.
+ */
+package sudoku;
+
+
+public class Main {
+
+    /* Invoked at runtime, starts and runs the program. */
+    public static void main(String[] args) {}
+
+}
