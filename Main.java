72b530b475ee0001f153367f77ff1d6e7eac4c8b
cvikupitz <cvikupit@uoregon.edu>
2016-11-02 21:14:10 -0700

    Drew lines, game is playable, but closes after winning

--- Main.java
+++ Main.java
@@ -30,7 +30,7 @@ public class Main {
 //        System.out.printf("%d puzzles scanned. %d complete.\n", i, j);
 
 
-/* Set the Nimbus look and feel */
+        /* Set the Nimbus look and feel */
         //<editor-fold defaultstate="collapsed" desc=" Look and feel setting code (optional) ">
         /* If Nimbus (introduced in Java SE 6) is not available, stay with the default look and feel.
          * For details see http://download.oracle.com/javase/tutorial/uiswing/lookandfeel/plaf.html
