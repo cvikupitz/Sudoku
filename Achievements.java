416539de494ff69eb4f60de55b875c67d5e91809
cvikupitz <cvikupit@uoregon.edu>
2016-12-12 16:19:41 -0800

    Added AchievementsFrame.java and Achievements.java

---
+++ Achievements.java
@@ -0,0 +1,18 @@
+/**
+ * Achievements.java
+ * Authors: Lucas Chavarria, Cole Vikupitz, Ron Guo, James Xu
+ * -----------------------------------------------------------------------------
+ * FIXME
+ */
+package sudoku;
+
+
+public class Achievements {
+
+    protected static int[] puzzles = {0, 0, 0, 0, 0, 0};
+    protected static boolean[] competitive = {false, false, false, false, false};
+    protected static int[] handicapped = {0, 0, 0, 0, 0};
+    protected static int[] assistant = {0, 0};
+    protected static boolean[] engineer = {false, false, false, false, false,
+                                           false, false};
+} // End Achievements class
