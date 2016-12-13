38e3eac229e1f0355b4f3f9c507e3892db64fa38
cvikupitz <cvikupit@uoregon.edu>
2016-12-12 16:24:41 -0800

    Modified Achievements.java.

--- Achievements.java
+++ Achievements.java
@@ -15,4 +15,16 @@ public class Achievements {
     protected static int[] assistant = {0, 0};
     protected static boolean[] engineer = {false, false, false, false, false,
                                            false, false};
+
+    /***/
+    protected void reset() {
+        Achievements.puzzles = new int[]{0, 0, 0, 0, 0, 0};
+        Achievements.competitive = new boolean[]{false, false, false, false, false};
+        Achievements.handicapped = new int[]{0, 0, 0, 0, 0};
+        Achievements.assistant = new int[]{0, 0};
+        Achievements.engineer = new boolean[]{false, false, false, false, false,
+                                              false, false};
+    }
+
+    
 } // End Achievements class
